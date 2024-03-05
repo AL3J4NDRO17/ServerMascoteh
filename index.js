63bac6383608d853b5248e792d778d9f8b17d90c
AL3J4NDRO17 <116526021+AL3J4NDRO17@users.noreply.github.com>
2024-03-04 23:22:22 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3000;
+
+// Configurar middleware para analizar el cuerpo de las solicitudes HTTP
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// URL de conexión a tu base de datos MongoDB Atlas
+const mongoUrl = "mongodb+srv://pixon:Filo1234@cluster0.sw8tbcs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Ruta para recibir datos desde la ESP32
+app.post('/app/application-0-laqjr/endpoint/SensorData', async (req, res) => {
+  const data = req.body;
+
+  try {
+    // Conectar a la base de datos MongoDB Atlas
+    const client = await MongoClient.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conexión exitosa a MongoDB Atlas");
+
+    // Obtener una referencia a la base de datos y la colección
+    const db = client.db("SensorData");
+    const collection = db.collection("DeviceState");
+
+    // Insertar los datos en la colección
+    await collection.insertOne(data);
+    console.log("Datos insertados en la base de datos");
+
+    // Cerrar la conexión
+    client.close();
+    console.log("Conexión cerrada");
+
+    // Responder a la ESP32 con un mensaje de confirmación
+    res.send("Datos recibidos y guardados en la base de datos");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB Atlas:", error);
+    res.status(500).send("Error al conectar a la base de datos");
+  }
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor Node.js escuchando en http://localhost:${port}`);
+});
